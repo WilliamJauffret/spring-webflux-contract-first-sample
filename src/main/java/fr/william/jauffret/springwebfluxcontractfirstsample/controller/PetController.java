7c31ecc375b93eaf2c1f4092c85518be0d0fddf3
WilliamJauffret <jauffret.william@gmail.com>
2023-04-30 15:15:47 +0200

    feat: refacto packages

--- src/main/java/fr/william/jauffret/springwebfluxcontractfirstsample/controller/PetController.java
+++ src/main/java/fr/william/jauffret/springwebfluxcontractfirstsample/controller/PetController.java
@@ -1,7 +1,7 @@
 package fr.william.jauffret.springwebfluxcontractfirstsample.controller;
 
-import fr.william.jauffret.springreactiveopenapicodegen.api.v1.PetApi;
-import fr.william.jauffret.springreactiveopenapicodegen.dto.PetDto;
+import fr.william.jauffret.springwebfluxcontractfirstsample.api.v1.PetApi;
+import fr.william.jauffret.springwebfluxcontractfirstsample.dto.PetDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 @Validated
 @RequestMapping("/api/v1")
 public class PetController implements PetApi {
+
     @Override
     public Mono<ResponseEntity<Flux<PetDto>>> getPets(ServerWebExchange exchange) {
         return PetApi.super.getPets(exchange);
