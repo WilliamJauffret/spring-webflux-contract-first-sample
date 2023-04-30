1dfea2a34e3fc05da97fe7b4fe832ffe818a343c
WilliamJauffret <jauffret.william@gmail.com>
2023-04-30 15:22:42 +0200

    feat: implements petdto flux

--- src/main/java/fr/william/jauffret/springwebfluxcontractfirstsample/controller/PetController.java
+++ src/main/java/fr/william/jauffret/springwebfluxcontractfirstsample/controller/PetController.java
@@ -10,6 +10,9 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.OffsetDateTime;
+import java.util.stream.Stream;
+
 @RestController
 @Validated
 @RequestMapping("/api/v1")
@@ -17,6 +20,11 @@ public class PetController implements PetApi {
 
     @Override
     public Mono<ResponseEntity<Flux<PetDto>>> getPets(ServerWebExchange exchange) {
-        return PetApi.super.getPets(exchange);
+        PetDto petDto1 = new PetDto();
+        petDto1.setPetId(1);
+        petDto1.setName("William");
+        petDto1.setBirthDate(OffsetDateTime.now());
+        Flux<PetDto> flux = Flux.fromStream(Stream.of(petDto1, petDto1, petDto1));
+        return Mono.just(ResponseEntity.ok(flux));
     }
 }
